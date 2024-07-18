9ab0cb074fa7b4e61259dd7a6b8e0655c1010c81
Vivek Shinde <vivekshinde@google.com>
2024-07-18 15:04:03 -0500

    Update bqload.sh

--- bqload.sh
+++ bqload.sh
@@ -2,6 +2,6 @@ bq load \
 --source_format=ORC \
 --time_partitioning_field=tripStartTimestamp --time_partitioning_type=MONTH \
 loadbq.newparttbl \
-gs://testdata7/bqtest/202401/modified/*.orc
-gs://testdata7/bqtest/202402/modified/*.orc
+gs://testdata7/bqtest/202401/modified/*.orc \
+gs://testdata7/bqtest/202402/modified/*.orc \
 gs://testdata7/bqtest/202403/modified/*.orc
