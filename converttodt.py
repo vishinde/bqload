58ebeae67f9a2ce56e9d981558afbfb00c246f41
Vivek Shinde <vivekshinde@google.com>
2024-07-18 14:52:56 -0500

    Update converttodt.py

--- converttodt.py
+++ converttodt.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import to_timestamp, add_months, col
+from pyspark.sql.functions import to_timestamp
 
 spark = SparkSession.builder.getOrCreate()
 
